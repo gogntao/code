a74b7720cbd55f0da9536d6b88d1467760d88b68
龚涛 <gongtao@gogntao.local>
2017-05-05 00:51:23 +0800

    Modify on poj2488

--- algorithm/search/POJ2488/POJ2488.cpp
+++ algorithm/search/POJ2488/POJ2488.cpp
@@ -9,15 +9,26 @@ int row, col, count;
 typedef struct path_{
     char x;
     int y;
-}path;
-int path[SIZE*SIZE];
+}path_;
+path_ path[SIZE*SIZE];
+
+int check(){
+    int sum = 0;
+    for(int i = 1; i <= row; ++i){
+        for(int j = 1; j < col; ++j)
+            sum += visit[i][j];
+    }
+    if(sum == row * col) return true;
+    else return false;
+}
+
 
 int travel(int i, int j){
     if(i <= 0 | j <= 0 | i > row | j > col) return false;
     if(check()) return true;
     if(visit[i][j] == 1) return false;
     else{
-        visit[i][j] == 1;
+        visit[i][j] = 1;
         count++;
         path[count].x = alp[row-1];
         path[count].y = col;
@@ -30,21 +41,8 @@ int travel(int i, int j){
         travel(i-2, j-1);
         travel(i-1, j-2);
         visit[i][j] = 0;
-        path[count].x = 0;
-        path[count].y = 0;
         count--;
     }
-}
-
-int check(){
-    int sum = 0;
-    for(int i = 1; i <= row; ++i){
-        for(int j = 1; j < col; ++j)
-            sum += visit[i][j];
-        }
-    }
-    if(sum == row * col) return true;
-    else return false;
 }
 
 int init(){
@@ -65,7 +63,7 @@ int main(){
         init();
         printf("Scenario: #%d:\n", c);
         for(int i = 1; i <= row; ++i){
-            for(int j = 1; j <=row; ++j){
+            for(int j = 1; j <=col; ++j){
                 if(travel(i, j)){
                     for(int k = 0; k < count; ++k){
                         printf("%c%d", path[k].x, path[k].y);
