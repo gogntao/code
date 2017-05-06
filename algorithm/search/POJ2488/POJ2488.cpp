4249f5e81618fea4b83e450b77b590fb366cbb73
龚涛 <gongtao@gogntao.local>
2017-05-06 23:02:32 +0800

    New modify , but not work!" ;
    "

--- algorithm/search/POJ2488/POJ2488.cpp
+++ algorithm/search/POJ2488/POJ2488.cpp
@@ -2,15 +2,15 @@
 #include <cstring>
 #include <cstdlib>
 
-const int SIZE = 30;
+const int MAX_SIZE = 30;
 const char alp[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-int visit[SIZE][SIZE];
-int row, col, count;
+int visit[MAX_SIZE][MAX_SIZE];
+int row, col, count, mode = 0;
 typedef struct path_{
     char x;
     int y;
 }path_;
-path_ path[SIZE*SIZE];
+path_ path[MAX_SIZE*MAX_SIZE];
 
 int check(){
     int sum = 0;
@@ -18,19 +18,26 @@ int check(){
         for(int j = 1; j < col; ++j)
             sum += visit[i][j];
     }
+    // std::cout << sum << std::endl;
     if(sum == row * col) return true;
     else return false;
 }
 
 
 int travel(int i, int j){
-    if(i <= 0 | j <= 0 | i > row | j > col) return false;
-    if(check()) return true;
-    if(visit[i][j] == 1) return false;
-    else{
+    // if(i <= 0 | j <= 0 | i > row | j > col) return false;
+    // if(visit[i][j] != 1) visit[i][j] = 1;
+    // else return false;
+    if(check()) {
+        std::cout << i + 'A' << j;
+        mode = 1;
+        return true;
+    }
+    
+    if(i > 0 && j > 0 && i <= row && j <=col && visit[i][j] != 1){
         visit[i][j] = 1;
         count++;
-        path[count].x = alp[row-1];
+        path[count].x = alp[i-1];
         path[count].y = col;
         travel(i+2, j+1);
         travel(i+1, j+2);
@@ -41,7 +48,6 @@ int travel(int i, int j){
         travel(i-2, j-1);
         travel(i-1, j-2);
         visit[i][j] = 0;
-        count--;
     }
 }
 
@@ -51,8 +57,9 @@ int init(){
             visit[i][j] = 0;
         }
     }
-    memset(path, 0, sizeof(path)*SIZE*SIZE);
+    memset(path, 0, sizeof(path_)*MAX_SIZE*MAX_SIZE);
     count = 0;
+    return 0;
 }
 
 int main(){
@@ -61,17 +68,14 @@ int main(){
     for(int c = 1; c <= testCase; ++c){
         std::cin >> row >> col;
         init();
-        printf("Scenario: #%d:\n", c);
+        // printf("Scenario: #%d:\n", c);
+        std::cout << "Scenario: #:" << c <<std::endl;
         for(int i = 1; i <= row; ++i){
             for(int j = 1; j <=col; ++j){
-                if(travel(i, j)){
-                    for(int k = 0; k < count; ++k){
-                        printf("%c%d", path[k].x, path[k].y);
-                    }
-                }
+                travel(i, j);
             }
         }
-        if(!check()) printf("impossible");
-        printf("\n\n");
+        if(mode == 0) std::cout << "impossible";
+        std::cout << std::endl << std::endl;
     }
 }
