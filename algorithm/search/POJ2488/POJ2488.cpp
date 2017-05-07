4dba8a0c21095df0a1c7dfce01d25447ca9823fd
龚涛 <gongtao@gogntao.local>
2017-05-07 17:48:41 +0800

    An answer suit a small case of test

--- algorithm/search/POJ2488/POJ2488.cpp
+++ algorithm/search/POJ2488/POJ2488.cpp
@@ -5,12 +5,12 @@
 const int MAX_SIZE = 30;
 const char alp[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 int visit[MAX_SIZE][MAX_SIZE];
-int row, col, mode = 0;
-// typedef struct path_{
-//     char x;
-//     int y;
-// }path_;
-// path_ path[MAX_SIZE*MAX_SIZE];
+int row, col, mode = 0, count = 0;
+typedef struct path_{
+    char x;
+    int y;
+}path_;
+path_ path[MAX_SIZE*MAX_SIZE];
 
 int check(){
     int sum = 0;
@@ -28,61 +28,76 @@ int travel(int i, int j){
     // if(i <= 0 | j <= 0 | i > row | j > col) return false;
     // if(visit[i][j] != 1) visit[i][j] = 1;
     // else return false;
-    if(i > 0 && j > 0 && i <= row && j <= col && visit[i][j] != 1){
+    if(i > 0 && j > 0 && i <= row && j <=col && visit[i][j] != 1){
         visit[i][j] = 1;
-        // count++;
-        // path[count].x = alp[i-1];
-        // path[count].y = col;
-        travel(i+2, j+1);
+        travel(i-1, j-2);
         if(check()) {
-            std::cout << alp[j-1] << i;
-            mode = 1;
-            return true;
-        }
-        travel(i+1, j+2);
-        if(check()) {
-            std::cout << alp[j-1] << i;
-            mode = 1;
-            return true;
-        }
-        travel(i-2, j+1);
-        if(check()) {
-            std::cout << alp[j-1] << i;
-            mode = 1;
-            return true;
-        }
-        travel(i-1, j+2);
-        if(check()) {
-            std::cout << alp[j-1] << i;
-            mode = 1;
-            return true;
-        }
-        travel(i+2, j-1);
-        if(check()) {
-            std::cout << alp[j-1] << i;
+            count++;
+            path[count].x = alp[j-1];
+            path[count].y = i;
             mode = 1;
             return true;
         }
         travel(i+1, j-2);
         if(check()) {
-            std::cout << alp[j-1] << i;
+            count++;
+            path[count].x = alp[j-1];
+            path[count].y = i;
             mode = 1;
             return true;
         }
         travel(i-2, j-1);
         if(check()) {
-            std::cout << alp[j-1] << i;
+            count++;
+            path[count].x = alp[j-1];
+            path[count].y = i;
             mode = 1;
             return true;
         }
-        travel(i-1, j-2);
+        travel(i+2, j-1);
         if(check()) {
-            std::cout << alp[j-1] << i;
+            count++;
+            path[count].x = alp[j-1];
+            path[count].y = i;
             mode = 1;
             return true;
         }
+        travel(i-2, j+1);
+        if(check()) {
+            count++;
+            path[count].x = alp[j-1];
+            path[count].y = i;
+            mode = 1;
+            return true;
+        }
+        travel(i+2, j+1);
+        if(check()) {
+            count++;
+            path[count].x = alp[j-1];
+            path[count].y = i;
+            mode = 1;
+            return true;
+        }
+        travel(i-1, j+2);
+        if(check()) {
+            count++;
+            path[count].x = alp[j-1];
+            path[count].y = i;
+            mode = 1;
+            return true;
+        }
+        travel(i+1, j+2);
+        if(check()) {
+            count++;
+            path[count].x = alp[j-1];
+            path[count].y = i;
+            mode = 1;
+            return true;
+        }
+        
         visit[i][j] = 0;
     }
+    return mode;
 }
 
 int init(){
@@ -91,8 +106,8 @@ int init(){
             visit[i][j] = 0;
         }
     }
-    // memset(path, 0, sizeof(path_)*MAX_SIZE*MAX_SIZE);
-    // count = 0;
+    memset(path, 0, sizeof(path_)*MAX_SIZE*MAX_SIZE);
+    count = 0;
     mode = 0;
     return 0;
 }
@@ -105,12 +120,13 @@ int main(){
         init();
         // printf("Scenario: #%d:\n", c);
         std::cout << "Scenario: #:" << c <<std::endl;
-        for(int j = 1; j <= col; ++j){
-            for(int i = 1; i <= row; ++i){
+        for(int i = 1; i <= row; ++i){
+            for(int j = 1; j <=col; ++j){
                 travel(i, j);
             }
         }
         // travel(1, 1);
+        for(int i = count ; i >= 1; --i) std::cout << path[i].x << path[i].y;
         if(mode == 0) std::cout << "impossible";
         std::cout << std::endl << std::endl;
     }
